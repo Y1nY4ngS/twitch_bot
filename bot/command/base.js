dfd1ffdceef79879081ffda19fa6ba65b3e1b918
Quoc Hung Nguyen <quochungnguyen1105@gmail.com>
2025-11-04 12:24:58 +0100

    Random number game

--- bot/command/base.js
+++ bot/command/base.js
@@ -1,17 +1,17 @@
-const age = require('../../bot/command/age');
+const age = require('../command/age');
+const randomNumber = require('../command/randomNumber');
 
 module.exports = {
     help: {
         response: (username)=>
-            `@${username}, here are the available commands:\n!help: List of all commands\n!info: Some info about me\n!wp: List of available weapon builds\n!<weapon_name>: Get the build code for the specified weapon.`,
+            `@${username}, here are the available commands:\n!help: List of all commands\n!info: Some info about me\n!random <1-10>: Random number game\n!wp: List of available weapon builds\n!<weapon_name>: Get the build code for the specified weapon.`,
     },
     info: {
         response: (username)=>
             `@${username}, my name is Hung, I'm ${age(process.env.BOD)} and currently I'm in Switzerland.`,
   },
-    randomNumber: {
-        response: (username =>
-            `@${username}, your number was ${usernumber} a`
-        )
+    random: {
+        response: (username, argument) =>
+            `@${username},${randomNumber(argument)}`,
     }
 };
---
+++ bot/command/randomNumber.js
@@ -0,0 +1,23 @@
+function randomNumber(userNumber) {
+
+    if (!userNumber) {
+        return `Please enter a valid input example !random 5`
+    }
+    const userNum = Number(userNumber)
+    
+    if (!Number.isInteger(userNum) || userNum < 1 || userNum > 10) {
+        return `Unvalid input, please pick a number between 1 and 10`
+    }
+
+    const num = Math.floor(Math.random() * 10) + 1
+    let string = ``
+
+    if (userNum === num) {
+        string = `Congratulations, the number ${userNum} was correct`
+    } else {
+        string = `Sadly the number was ${num}`
+    }
+    return string;
+}
+
+module.exports = randomNumber;
